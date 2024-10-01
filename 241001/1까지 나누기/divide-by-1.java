ee8f0b4a811c954c645d6f51e0cdea24e5dc127a
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-01 19:52:37 +0900

    Title: 1까지 나누기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241001/1까지 나누기/divide-by-1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while (true) {
+            if (n <= 1) {
+                break;
+            } else {
+                n /= i;
+                i++;   
+            }
+        }
+        System.out.print(i-1);
+    }
+}
