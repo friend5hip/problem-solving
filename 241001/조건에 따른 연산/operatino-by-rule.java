f38fedfe6bd2464eef0499b971227718b8efaff0
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-01 20:13:28 +0900

    Title: 조건에 따른 연산, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241001/조건에 따른 연산/operatino-by-rule.java
+++ 241001/조건에 따른 연산/operatino-by-rule.java
@@ -9,12 +9,11 @@ public class Main {
         while (n < 1000) {
             if (n % 2 == 0) {
                 n = (n * 3) + 1;
-                cnt++;
             }
             else {
                 n = (n * 2) + 2;
-                cnt++;
             }
+            cnt++;
         } 
         System.out.println(cnt);
     }
