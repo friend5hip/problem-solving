f843c7c791371bc6f074b3e5a15b32237a2faf9d
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-07 12:37:33 +0900

    Title: 진수 to 진수, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241007/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static int aToDecimal(int num, int a) {
+        int digit = 1;
+        int decimal = 0;
+        while (num > 0) {
+            int tmp = num % 10;
+            decimal += tmp * digit;
+            digit *= a;
+            num /= a;
+        }
+        return decimal;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+
+        int aToDec = aToDecimal(n, a);
+        ArrayList<Integer> result = new ArrayList<>();
+        while (aToDec > 0) {
+            int tmp = aToDec % b;
+            result.add(tmp);
+            aToDec /= b;
+        }
+        Collections.reverse(result);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i));
+        }
+    }
+}
