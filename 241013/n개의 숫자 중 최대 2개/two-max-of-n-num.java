abade7c7a95aa2c5fed6283db8bc42d082fc14d3
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-13 15:41:45 +0900

    Title: n개의 숫자 중 최대 2개, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 241013/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 241013/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
