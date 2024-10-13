a512d5bd86edb136a6267bb24203d95d522783f2
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-13 14:41:34 +0900

    Title: n개의 숫자 중 최소, Time: 225ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241013/n개의 숫자 중 최소/min-of-n-num.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final int INT_MAX = Integer.MAX_VALUE;
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int minVal = INT_MAX;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (minVal > arr[i]) {
+                minVal = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == minVal) {
+                cnt++;
+            }
+        }
+        System.out.print(minVal + " " + cnt);
+    }
+}
