9dbfa650a7258e0fc378eab39e453aef0366f960
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-15 16:05:09 +0900

    Title: 아래로 사각형 채우기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241015/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] map = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            int num = i + 1;
+            for (int j = 0; j < n; j++) {
+                map[i][j] = num + j * n;
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
