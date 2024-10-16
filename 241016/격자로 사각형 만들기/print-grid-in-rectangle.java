57285f7f88cd40e91ee2a2214019f41c5721cd56
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-16 09:24:12 +0900

    Title: 격자로 사각형 만들기, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 241016/격자로 사각형 만들기/print-grid-in-rectangle.java
+++ 241016/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException; 
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
             Arrays.fill(arr[i], 1);
