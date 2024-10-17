775502061d744b84c0294fd0ca239e6224caa4b7
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-17 09:11:27 +0900

    Title: 동전이 있는 위치, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241017/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] grid = new int[n+1][n+1];
+        for (int i = 0; i < m; i++) {
+            String t = br.readLine();
+            StringTokenizer st2 = new StringTokenizer(t);
+            int r = Integer.parseInt(st2.nextToken());
+            int c = Integer.parseInt(st2.nextToken());
+
+            grid[r-1][c-1] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
