681ce22b700db69950a36edc220ef73c0140c30a
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-28 09:21:05 +0900

    Title: 더 긴 문자열, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241028/더 긴 문자열/longer-string.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        String word1 = st.nextToken();
+        String word2 = st.nextToken();
+
+        if (word1.length() > word2.length()) {
+            System.out.println(word1 + " " + word1.length());
+        } else if (word1.length() == word2.length()) {
+            System.out.println("same");
+        } else {
+            System.out.println(word2 + " " + word2.length());
+        }
+    }
+}
