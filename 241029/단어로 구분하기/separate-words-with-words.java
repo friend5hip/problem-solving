9fa5de883127f811723affb134df6c47dadfc72e
friend5hip <107809044+friend5hip@users.noreply.github.com>
2024-10-29 10:44:25 +0900

    Title: 단어로 구분하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 241029/단어로 구분하기/separate-words-with-words.java
+++ 241029/단어로 구분하기/separate-words-with-words.java
@@ -4,9 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-     
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
         for (int i = 0; i < 10; i++) {
             System.out.println(st.nextToken());
