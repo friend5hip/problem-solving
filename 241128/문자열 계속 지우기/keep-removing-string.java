7ea711cce20d638b41c9cee6d712b011bdae3a8d
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2024-11-28 17:24:43 +0900

    Title: 문자열 계속 지우기, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241128/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // a, b를 입력받는다
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        // B가 A의 부분 문자열이 아닐 때까지 반복
+        while (a.contains(b)) {
+            // A에서 B를 제거
+            int index = a.indexOf(b);
+            a = a.substring(0, index) + a.substring(index + b.length(), a.length());
+        }
+        System.out.println(a);
+    }
+}
