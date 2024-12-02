d4ded4c6e7c5bd979bccda732839cb7c06681613
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2024-12-02 10:47:10 +0900

    Title: 그 다음 알파벳, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

--- 241202/그 다음 알파벳/next-alphabet.java
+++ 241202/그 다음 알파벳/next-alphabet.java
@@ -9,7 +9,7 @@ public class Main {
         // a에 1을 더해 다음 문자를 얻는다. (a: 91, z: 117)
         // z를 넘어가면 26을 빼서 z 다음은 a
         int aNext = ((int) a.charAt(0) + 1);
-        if (aNext > 117) {
+        if (aNext > 121) {
             aNext -= 26;
         }
 
