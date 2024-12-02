1df2c8433f2f12108b6ebb5280b4b774720bba6d
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2024-12-02 10:44:27 +0900

    Title: 그 다음 알파벳, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241202/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 영문자 a를 입력받는다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+
+        // a에 1을 더해 다음 문자를 얻는다. (a: 91, z: 117)
+        // z를 넘어가면 26을 빼서 z 다음은 a
+        int aNext = ((int) a.charAt(0) + 1);
+        if (aNext > 117) {
+            aNext -= 26;
+        }
+
+        // 출력
+        System.out.println((char) aNext);
+    }
+
+}
