24e4502a56bc9ef8dc8f44b3ee28db555b5d268a
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2024-12-04 20:59:31 +0900

    Title: 그래프 탐색, Time: 192ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241204/그래프 탐색/graph-traversal.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 노드 방문 회수
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        // N개의 노드와 M개의 간선을 입력 받음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        // 그래프를 작성
+        boolean[] visited = new boolean[N+1];
+        int[][] graph = new int[N+1][N+1];
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st2.nextToken());
+            int y = Integer.parseInt(st2.nextToken());
+            graph[x][y] = 1;
+            graph[y][x] = 1;
+        }
+        
+        // 그래프를 깊이 우선 탐색
+        DFS(1, N, graph, visited);
+        
+        // 방문할 수 있는 노드를 탐색한 횟수를 출력
+        System.out.println(count);
+    }
+
+    static void DFS(int vertex, int vertices, int[][] graph, boolean[] visited) {
+        visited[vertex] = true;
+        for (int currV = 1; currV <= vertices; currV++) {
+            if (graph[vertex][currV] == 1 && !visited[currV]) {
+                count++;
+                visited[currV] = true;
+                DFS(currV, vertices, graph, visited);
+            }
+        }
+    }
+
+}
