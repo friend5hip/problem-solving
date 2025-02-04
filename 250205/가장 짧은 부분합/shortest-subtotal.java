d44583c25164031e405baca45d0f675f9ac2fa2f
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-02-05 00:06:22 +0900

    Title: 가장 짧은 부분합, Time: 89ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 250205/가장 짧은 부분합/shortest-subtotal.java
+++ 250205/가장 짧은 부분합/shortest-subtotal.java
@@ -21,18 +21,21 @@ public class Main {
         int minLength = MAX_VALUE;
         int sum = 0;
         int j = 0;
+        // 두 개의 포인터로 합을 계산
         for (int i = 1; i <= n; i++) {
-            if (sequence[j + 1] > s) {
+            // 첫번째 포인터가 s보다 작은 경우 다음 포인터로 전진
+            if (sequence[i] < s) {
                 break;
             }
-
+            // 더할 숫자가 남아 있고, 현재까지 합과 다음 숫자의 합이 s보다 크지 않을 경우
+            // 더한 뒤 두번째 포인터 전진
             while (j + 1 <= n && !(sum + sequence[j + 1] >= s)) {
                 sum += sequence[j + 1];
                 j++;
             }
-
+            // 최소 길이를 계산
             minLength = Math.min(minLength, (j+1) - i + 1);
-
+            // 첫번째 포인터의 숫자를 제외
             sum -= sequence[i];
         }
 
