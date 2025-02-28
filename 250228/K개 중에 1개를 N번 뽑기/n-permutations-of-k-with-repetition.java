841ca97e4e30f6b3d24d8155698d524db627f07c
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-02-28 12:50:05 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 282ms, Memory: 20MB, Status: Passed - CodeTree

--- 250228/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
+++ 250228/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,37 +1,30 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
-    static int k, n;
-    static ArrayList<Integer> answer = new ArrayList<>();
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        k = sc.nextInt();
-        n = sc.nextInt();
-        
-        choose(1);
-    }
+        int k = sc.nextInt();
+        int n = sc.nextInt();
 
-    static void choose(int currNum) {
-        // n개를 선택한 경우 결과 출력
-        if (currNum == n + 1) {
-            printAnswer();
-            return;
-        }
-        
-        // k 이하의 숫자를 골라 재귀 호출
-        for (int select = 1; select <= k; select++) {
-            answer.add(select);
-            choose(currNum + 1);
-            answer.remove(answer.size() - 1); 
-        }
-    }
+        int[] answer = new int[n];
+        StringBuilder sb = new StringBuilder();
 
-    static void printAnswer() {
-        for (int i = 0; i < answer.size(); i++) {
-            System.out.print(answer.get(i) + " ");
+        int count = (int) Math.pow(k, n); // 총 경우의 수
+
+        for (int i = 0; i < count; i++) {
+            int num = i;
+            for (int j = 0; j < n; j++) {
+                answer[j] = (num % k) + 1; // 숫자 선택
+                num /= k;
+            }
+
+            // 결과 출력
+            for (int j = n - 1; j >= 0; j--) {
+                sb.append(answer[j]).append(" ");
+            }
+            sb.append("\n");
         }
-        System.out.println();
+
+        System.out.print(sb.toString());
     }
 }
