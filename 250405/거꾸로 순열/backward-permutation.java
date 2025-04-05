9ab9f3a5aa464317d02facd49c1896b3a66abf6e
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-04-05 17:21:04 +0900

    Title: 거꾸로 순열, Time: 1008ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250405/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static ArrayList<Integer> answer = new ArrayList<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        visited = new boolean[n+1];
+        getPermutation(0);
+    }
+
+    static void getPermutation(int depth) {
+        if (depth == n) {
+            printAnswer();
+            return;
+        }
+
+        for (int i = n; i >= 1; i--) {
+            if (!visited[i]) {
+                visited[i] = true;
+                answer.add(i);
+                getPermutation(depth + 1);
+                answer.remove(answer.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    static void printAnswer() {
+        for (int num : answer) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
