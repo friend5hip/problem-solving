9ff17b7b78013f902ac6172a3e16beb1e46a0fbd
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-04-05 18:38:14 +0900

    Title: 외판원 순회, Time: 175ms, Memory: 11MB, Status: Passed - Codetree

--- 250405/외판원 순회/traveling-salesman-problem.java
+++ 250405/외판원 순회/traveling-salesman-problem.java
@@ -10,22 +10,20 @@ public class Main {
         n = sc.nextInt();
         cost = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 cost[i][j] = sc.nextInt();
-            }
-        }
 
         visited = new boolean[n];
-        for (int start = 0; start < n; start++) {
-            visited[start] = true;
-            getMin(start, start, 1, 0);
-            visited[start] = false;
-        }
+        visited[0] = true; // 0번 도시에서 시작
+        getMin(0, 0, 1, 0);
+
         System.out.println(min);
     }
 
     static void getMin(int start, int curr, int depth, int currentSum) {
+        if (currentSum >= min) return;
+
         if (depth == n) {
             if (cost[curr][start] != 0) {
                 min = Math.min(min, currentSum + cost[curr][start]);
