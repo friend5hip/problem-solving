c62e5ae31f3d9595c59a50779f4b94087d481d66
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-04-07 23:17:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 67.70 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,27 +1,23 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
+    public List<Integer> solution(int []arr) {
         Queue<Integer> queue = new ArrayDeque<>();
         for (int num : arr) {
             queue.add(num);
         }
 
         int current = queue.poll();
-        List<Integer> list = new ArrayList<>();
-        list.add(current);
+        List<Integer> answer = new ArrayList<>();
+        answer.add(current);
         while (!queue.isEmpty()) {
             int next = queue.poll();
             if (current != next) {
-                list.add(next);
+                answer.add(next);
             }
             current = next;
         }
         
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
         return answer;
     }
 }
