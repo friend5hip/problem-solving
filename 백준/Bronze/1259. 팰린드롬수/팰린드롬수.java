3ad10a215ffca474c3fdd0e57ac5b51d7e5e0816
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-04-10 00:52:07 +0900

    [Bronze I] Title: 팰린드롬수, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -6,26 +6,11 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         while (true) {
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) break;
-            String numToStr = String.valueOf(num);
-            char[] charArr = numToStr.toCharArray();
-            char[] charArr2 = new char[charArr.length];
-            int j = 0;
-            for (int i = charArr.length - 1; i >= 0; i--) {
-                charArr2[j] = charArr[i];
-                j++;
-            }
-            StringBuilder reversed = new StringBuilder();
-            for (char ch : charArr2) {
-                reversed.append(ch);
-            }
-            if (num == Integer.parseInt(reversed.toString())) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
-
+            String num = br.readLine();
+            if (num.equals("0")) break;
+            StringBuilder sb = new StringBuilder(num);
+            String reversed = String.valueOf(sb.reverse());
+            System.out.println(num.equals(reversed) ? "yes" : "no");
         }
     }
 }
