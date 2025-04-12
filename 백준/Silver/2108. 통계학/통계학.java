513a60776483b1178b9127c9d5a95fe64ed36631
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-04-12 22:56:39 +0900

    [Silver II] Title: 통계학, Time: 596 ms, Memory: 63528 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,47 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
         int[] numbers = new int[n];
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
         int sum = 0;
+
         for (int i = 0; i < n; i++) {
-            int number = Integer.parseInt(br.readLine());
-            numbers[i] = number;
-            map.put(number, map.getOrDefault(number, 0) + 1);
-            sum += numbers[i];
+            int num = Integer.parseInt(br.readLine());
+            numbers[i] = num;
+            sum += num;
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
+
         Arrays.sort(numbers);
-        List<Map.Entry<Integer, Integer>> mapToList = new ArrayList<>(map.entrySet());
-        mapToList.sort((o1, o2) -> {
-            if (!Objects.equals(o1.getValue(), o2.getValue())) {
-                return o2.getValue() - o1.getValue();
-            } else {
-                return o1.getKey() - o2.getKey();
-            }
-        });
-        int average = (int) Math.round((double) sum / n);
-        int mid = numbers[(n - 1) / 2];
-        int maxFreq = mapToList.get(0).getValue();
-        List<Integer> frequentNumbers = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : mapToList) {
-            if (entry.getValue() == maxFreq) {
-                frequentNumbers.add(entry.getKey());
-            } else {
-                break;
+
+        double average = (double) sum / n;
+        System.out.println(Math.round(average));
+
+        System.out.println(numbers[n / 2]);
+
+        int maxFrequency = Collections.max(frequencyMap.values());
+        List<Integer> modes = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() == maxFrequency) {
+                modes.add(entry.getKey());
             }
         }
-        Collections.sort(frequentNumbers);
-        int range = Math.abs(numbers[0] - numbers[n - 1]);
-        System.out.println(average);
-        System.out.println(mid);
-        System.out.println(frequentNumbers.size() > 1 ? mapToList.get(1).getKey() : mapToList.get(0).getKey());
-        System.out.println(range);
+        Collections.sort(modes);
+        if (modes.size() > 1) {
+            System.out.println(modes.get(1));
+        } else {
+            System.out.println(modes.get(0));
+        }
+
+        System.out.println(numbers[n - 1] - numbers[0]);
     }
 }
