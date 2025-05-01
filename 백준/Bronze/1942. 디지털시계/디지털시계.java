afcafdf861b333ab3ff1375c9c9c1fd3727c2893
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-05-01 23:51:37 +0900

    [Bronze I] Title: 디지털시계, Time: 420 ms, Memory: 51068 KB -BaekjoonHub

--- 백준/Bronze/1942. 디지털시계/디지털시계.java
+++ 백준/Bronze/1942. 디지털시계/디지털시계.java
@@ -23,30 +23,27 @@ public class Main {
             endTime[2] = Integer.parseInt(st2.nextToken());
 
             int count = 0;
-            String end = String.valueOf(endTime[0]) + String.valueOf(endTime[1]) + endTime[2];
+            String end = String.valueOf(endTime[0]) + endTime[1] + endTime[2];
             if (endTime[1] < 10) end = endTime[0] + "0" + endTime[1] + endTime[2];
-            if (endTime[2] < 10) end = String.valueOf(endTime[0]) + String.valueOf(endTime[1]) + "0" + endTime[2];
-//            System.out.println("종료 시간: " + Integer.parseInt(end));
+            if (endTime[2] < 10) end = String.valueOf(endTime[0]) + endTime[1] + "0" + endTime[2];
             if (Integer.parseInt(end) % 3 == 0) count++;
 
             while (startTime[0] != endTime[0] || startTime[1] != endTime[1] || startTime[2] != endTime[2]) {
-                String current = String.valueOf(startTime[0]) + String.valueOf(startTime[1]) + startTime[2];
+                String current = String.valueOf(startTime[0]) + startTime[1] + startTime[2];
                 if (startTime[1] < 10) current = startTime[0] + "0" + startTime[1] + startTime[2];
-                if (startTime[2] < 10) current = String.valueOf(startTime[0]) + String.valueOf(startTime[1]) + "0" + startTime[2];
-//                System.out.println("현재 시간: " + current);
-//                System.out.println("현재 정수: " + Integer.parseInt(current));
+                if (startTime[2] < 10) current = String.valueOf(startTime[0]) + startTime[1] + "0" + startTime[2];
                 if (Integer.parseInt(current) % 3 == 0) count++; // 현재 수가 3의 배수면 카운트
-                // 초가 60을 초과하면 분을 1증가시키고 초는 0으로 만듦
+
                 startTime[2]++;
-                if (startTime[2] >= 60) {
+                if (startTime[2] > 59) {
                     startTime[1]++;
                     startTime[2] = 0;
                 }
-                if (startTime[1] >= 60) {
+                if (startTime[1] > 59) {
                     startTime[0]++;
                     startTime[1] = 0;
                 }
-                if (startTime[0] >= 24) {
+                if (startTime[0] > 23) {
                     startTime[0] = 0;
                     startTime[1] = 0;
                     startTime[2] = 0;
