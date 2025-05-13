1dc1b25c12b4b89efe2d6499ce57c2c9aabfb452
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-05-13 09:40:21 +0900

    [Silver III] Title: N과 M (1), Time: 340 ms, Memory: 47352 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,41 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static List<Integer> answer = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
     static int n, m;
-    static ArrayList<Integer> answer = new ArrayList<>();
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[n+1];
-        getPermutation(0);
+        visited = new boolean[n + 1];
+        backtracking(0);
+
+        System.out.println(sb);
     }
 
-    static void getPermutation(int currNum) {
-        if (currNum == m) {
-            printAnswer();
+    static void backtracking(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(answer.get(i) + " ");
+            }
+            sb.append("\n");
             return;
         }
-
+        
         for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
-                visited[i] = true;
-                answer.add(i);
-                getPermutation(currNum + 1);
-                answer.remove(answer.size() - 1);
-                visited[i] = false;
+               visited[i] = true;
+               answer.add(i);
+               backtracking(depth + 1);
+               answer.remove(answer.size() - 1);
+               visited[i] = false;
             }
         }
-    }
-
-    static void printAnswer() {
-        for (int num : answer) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
     }
 }
