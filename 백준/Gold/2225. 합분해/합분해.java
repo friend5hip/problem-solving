1ee2dcb5c1625c4ac8e2c67e44e434242ff99020
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-05-25 20:45:41 +0900

    [Gold V] Title: 합분해, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, k;
+    static final int MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        long[][] dp = new long[k + 1][n + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= k; i++) {
+            long sum = 0;
+            for (int j = 0; j <= n; j++) {
+                sum = (sum + dp[i - 1][j]) % MOD;
+                dp[i][j] = sum;
+            }
+        }
+
+        System.out.println(dp[k][n]);
+    }
+}
