b76d3e8cf60b00817b258ade31366fffbde32f2a
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-05-28 17:45:17 +0900

    [level 1] Title: 실패율, Time: 12.18 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,40 +1,30 @@
 import java.util.*;
 
 class Solution {
-    static class StageInfo {
-        int stage;
-        double failureRate;
-
-        StageInfo(int stage, double failureRate) {
-            this.stage = stage;
-            this.failureRate = failureRate;
-        }
-    }
-
     public int[] solution(int N, int[] stages) {
-        int[] currentUsers = new int[N + 2];
+        // 현재 스테이지를 도전중인 플레이어 수를 계산한다.
+        int[] currentUsers = new int[N + 2]; // 1 ~ N + 1까지 클리어한 사용자 수
         for (int stage : stages) {
             if (stage <= N) currentUsers[stage]++;
         }
-
+        
         int totalPlayers = stages.length;
         List<StageInfo> stageInfos = new ArrayList<>();
-
         for (int i = 1; i <= N; i++) {
             if (totalPlayers == 0) {
                 stageInfos.add(new StageInfo(i, 0));
             } else {
                 double rate = (double) currentUsers[i] / totalPlayers;
                 stageInfos.add(new StageInfo(i, rate));
-                totalPlayers -= currentUsers[i]; // 다음 스테이지로 넘어간 사람만 남김
+                totalPlayers -= currentUsers[i]; // 다음 스테이지로 넘어간 사람만 남긴다.
             }
         }
 
         stageInfos.sort((a, b) -> {
             if (Double.compare(b.failureRate, a.failureRate) != 0) {
-                return Double.compare(b.failureRate, a.failureRate); // 실패율 내림차순
+                return Double.compare(b.failureRate, a.failureRate); // 실패율을 기준으로 내림차순 정렬
             }
-            return Integer.compare(a.stage, b.stage); // 스테이지 번호 오름차순
+            return Integer.compare(a.stage, b.stage); // 스테이지 번호를 기준으로 오름차순 정렬
         });
 
         int[] result = new int[N];
@@ -43,5 +33,15 @@ class Solution {
         }
 
         return result;
+    }
+    
+    static class StageInfo {
+        int stage;
+        double failureRate;
+
+        StageInfo(int stage, double failureRate) {
+            this.stage = stage;
+            this.failureRate = failureRate;
+        }
     }
 }
