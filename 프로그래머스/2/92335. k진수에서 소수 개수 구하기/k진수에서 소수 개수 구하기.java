5ef45c1852e41d2b09c38ec1bd8026e98f1d5943
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-05-29 21:53:05 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 11.37 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -2,20 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k) {
-        String nBasedNum = getNBasedNumber(n, k);
+        String[] targets = getNBasedNumber(n, k).split("0");
         int count = 0;
-
-        int start = 0;
-        for (int end = 0; end <= nBasedNum.length(); end++) {
-            // 구간 끝이거나 0을 만나면 토큰 완성
-            if (end == nBasedNum.length() || nBasedNum.charAt(end) == '0') {
-                if (start < end) {
-                    String token = nBasedNum.substring(start, end);
-                    if (!token.contains("0") && isPrime(Long.parseLong(token))) {
-                        count++;
-                    }
-                }
-                start = end + 1; // 다음 구간 시작
+        // int start = 0;
+        // for (int end = 0; end <= nBasedNum.length(); end++) {
+        //     // 구간 끝이거나 0을 만나면 토큰 완성
+        //     if (end == nBasedNum.length() || nBasedNum.charAt(end) == '0') {
+        //         if (start < end) {
+        //             String token = nBasedNum.substring(start, end);
+        //             if (!token.contains("0") && isPrime(Long.parseLong(token))) {
+        //                 count++;
+        //             }
+        //         }
+        //         start = end + 1; // 다음 구간 시작
+        //     }
+        // }
+        for (String num : targets) {
+            if (num.equals("")) continue;
+            if (isPrime(Long.parseLong(num))) {
+                count++;
             }
         }
 
