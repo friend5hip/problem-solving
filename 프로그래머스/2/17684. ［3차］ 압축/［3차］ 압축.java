765ccd39902ecea9385538e57c5fb991cd0da3a5
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-05-30 13:26:59 +0900

    [level 2] Title: [3차] 압축, Time: 5.73 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String msg) {
+        // 인덱스 초기화
+        List<String> dictionary = new ArrayList<>();
+        dictionary.add("");
+        for (char c = 'A'; c <= 'Z'; c++) {
+            dictionary.add(String.valueOf(c));
+        }
+        
+        List<Integer> index = new ArrayList<>();
+        int start = 0;
+        while (start < msg.length()) {
+            int end = start + 1;
+            // 사전에 없는 단어가 나올 때까지 문자열 길이를 늘려가며 탐색
+            while (end <= msg.length() && dictionary.contains(msg.substring(start, end))) {
+                end++;
+            }
+            
+            // 사전에 있는 현재 단어까지 인덱스에 추가
+            String w = msg.substring(start, end - 1);
+            index.add(dictionary.indexOf(w));
+            
+            // 다음 글자를 추가하여 사전에 추가
+            if (end <= msg.length()) {
+                String wc = msg.substring(start, end);
+                dictionary.add(wc);
+            }
+            
+            start = end - 1;
+        }
+                    
+        return index;
+    }
+}
