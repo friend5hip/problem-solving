c34baa37922c77fc0be9b03664348c80e4543344
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-06-02 17:28:17 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18104 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,28 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int x;
-    static int[] dp = new int[1000001];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        x = Integer.parseInt(br.readLine());
-
-        for (int i = 2; i <= x; i++) {
+        int n = Integer.parseInt(br.readLine());
+        // i % 3 == 0 이면 / 3
+        // i % 2 == 0 이면 / 2
+        // - 1
+        // dp[i]는 i일 때, 1로 만들기 위한 최소 연산 횟수
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
-                dp[i] = dp[i / 2] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
-
-            if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
-                dp[i] = dp[i / 3] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-
         }
 
-        System.out.println(dp[x]);
+        System.out.println(dp[n]);
     }
 }
