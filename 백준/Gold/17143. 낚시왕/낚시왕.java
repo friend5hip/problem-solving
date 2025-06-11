246e625ef9a13c6d954acf59d44b778e52b00d80
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-06-11 14:42:39 +0900

    [Gold I] Title: 낚시왕, Time: 540 ms, Memory: 63128 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -3,17 +3,17 @@ import java.util.*;
 
 public class Main {
     static int r, c, m;
+    static Shark[][] map;
     static class Shark {
         int s, d, z;
         public Shark(int s, int d, int z) {
             this.s = s; this.d = d; this.z = z;
         }
     }
-    static Shark[][] map;
-    static int[] dx = {0, -1, 1, 0, 0}; // 상하우좌
+    static int[] dx = {0, -1, 1, 0, 0};
     static int[] dy = {0, 0, 0, 1, -1};
-    static int totalSize = 0;
-    static int UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
+    static final int UP = 1, DOWN = 2, RIGHT = 3, LEFT = 4;
+    static int totalZ = 0;
 
     public static void main(String[] args) throws IOException {
         // 칸에 상어는 최대 한 마리, 속도와 크기를 가진다.
@@ -27,99 +27,87 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken()); // 상어의 수
+        m = Integer.parseInt(st.nextToken());
         map = new Shark[r][c];
-        for (int i = 0; i < m; i++) {
+        while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken()) - 1;
             int c = Integer.parseInt(st.nextToken()) - 1;
             int s = Integer.parseInt(st.nextToken()); // 속도
-            int d = Integer.parseInt(st.nextToken()); // 이동 방향
+            int d = Integer.parseInt(st.nextToken()); // 방향
             int z = Integer.parseInt(st.nextToken()); // 크기
             map[r][c] = new Shark(s, d, z);
+
         }
 
         // c초 동안
         for (int i = 0; i < c; i++) {
             // 상어를 잡는다.
             catchShark(i);
-
-            // 상어가 이동한다.
+            // 상어가 움직인다.
             moveSharks();
-
-//            System.out.println("이동 후: ");
-//            for (int j = 0; j < r; j++) {
-//                for (int k = 0; k < c; k++) {
-//                    if (map[j][k] == null) System.out.print(0 + " ");
-//                    else System.out.print(map[j][k].z + " ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-
         }
 
-        System.out.println(totalSize);
+        System.out.println(totalZ);
     }
 
     static void catchShark(int col) {
         for (int i = 0; i < r; i++) {
             if (map[i][col] != null) {
-                totalSize += map[i][col].z;
+                totalZ += map[i][col].z;
                 map[i][col] = null;
-                break;
+                return;
             }
         }
     }
 
     static void moveSharks() {
-        Shark[][] temp = new Shark[r][c];
+        Shark[][] currentMap = new Shark[r][c];
 
         for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+            for (int j = 0; j < c; j++){
                 if (map[i][j] != null) {
-                    Shark shark = map[i][j];
-                    int x = i;
-                    int y = j;
-                    int s = shark.s;
-                    int d = shark.d;
-                    int z = shark.z;
-
-                    // 주기 계산
-                    if (d == UP || d == DOWN) {
-                        s %= (2 * (r - 1));
-                    } else {
-                        s %= (2 * (c - 1));
-                    }
-
-                    // s번 이동하되, 경계에 부딪히면 방향 전환
-                    for (int move = 0; move < s; move++) {
-                        int nx = x + dx[d];
-                        int ny = y + dy[d];
-
-                        if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
-                            if (d == UP) d = DOWN;
-                            else if (d == DOWN) d = UP;
-                            else if (d == LEFT) d = RIGHT;
-                            else if (d == RIGHT) d = LEFT;
-
-                            nx = x + dx[d];
-                            ny = y + dy[d];
-                        }
-
-                        x = nx;
-                        y = ny;
-                    }
-
-                    // 가장 큰 상어만 남김
-                    if (temp[x][y] == null || temp[x][y].z < z) {
-                        temp[x][y] = new Shark(shark.s, d, z);
+                    Shark current = map[i][j];
+                    int[] next = getNext(i, j, current.s, current.d);
+                    int nr = next[0], nc = next[1], nd = next[2];
+                    // 빈 칸 혹은 더 큰 경우 이동해서 잡아먹음
+                    if (currentMap[nr][nc] == null || currentMap[nr][nc].z < current.z) {
+                        currentMap[nr][nc] = new Shark(current.s, nd, current.z);
                     }
                 }
             }
         }
 
-        map = temp;
+        map = currentMap;
     }
 
+    static int[] getNext(int cr, int cc, int s, int d) {
+        if (d == UP || d == DOWN) {
+            s %= (2 * (r - 1));
+        } else {
+            s %= (2 * (c - 1));
+        }
+
+        for (int i = 0; i < s; i++) {
+            int nr = cr + dx[d];
+            int nc = cc + dy[d];
+            // 경계를 만나면 방향 전환
+            if (nr < 0 || nr >= r || nc < 0 || nc >= c) {
+                switch (d) {
+                    case UP: d = DOWN; break;
+                    case DOWN: d = UP; break;
+                    case LEFT: d = RIGHT; break;
+                    case RIGHT: d = LEFT; break;
+                }
+
+                nr = cr + dx[d];
+                nc = cc + dy[d];
+            }
+            
+            cr = nr;
+            cc = nc;
+        }
+        
+        return new int[]{cr, cc, d};
+    }
 }
