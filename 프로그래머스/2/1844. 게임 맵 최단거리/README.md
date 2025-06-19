0b68a4fb8681fa5575c43c4939f1838460dbc029
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-06-19 22:42:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 40.91 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 39.1 MB, 시간: 33.86 ms
+메모리: 39.4 MB, 시간: 40.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 19일 10:57:15
+2025년 06월 19일 22:42:05
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,41 +1,3 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  add(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.size++;
-  }
-
-  poll() {
-    if (!this.head) return null;
-    const removeNode = this.head;
-    this.head = this.head.next;
-    if (!this.head) {
-      this.tail = null;
-    }
-    this.size--;
-    return removeNode.value;
-  }
-}
-
 const dx = [1, -1, 0, 0];
 const dy = [0, 0, -1, 1];
 
@@ -46,12 +8,11 @@ function solution(maps) {
 }
 
 function bfs(x, y, maps, visited) {
-    const q = new Queue();
-    q.add([x, y, 1]);
+    const q = [[x, y, 1]];
     visited[x][y] = true;
     
-    while (q.size > 0) {
-        const [cx, cy, ct] = q.poll();
+    while (q.length > 0) {
+        const [cx, cy, ct] = q.shift();
         
         if (cx === maps.length - 1 && cy === maps[0].length - 1) return ct;
         
@@ -64,7 +25,7 @@ function bfs(x, y, maps, visited) {
         for (const [nx, ny] of moves) {
             if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length) continue;
             if (!visited[nx][ny] && maps[nx][ny] === 1) {
-                q.add([nx, ny, ct + 1]);
+                q.push([nx, ny, ct + 1]);
                 visited[nx][ny] = true;
             } 
         }
