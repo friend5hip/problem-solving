4baceafcb73e5ab0e4a468e7cb9e874f83b12703
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-06-19 11:53:42 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,23 @@
+function solution(n, computers) {
+    let count = 0;
+    const visited = Array(n).fill(false);
+    
+    function dfs(node) {
+        visited[node] = true;
+        
+        for (let i = 0; i < n; i++) {
+            if (!visited[i] && computers[node][i] === 1) {
+                dfs(i);
+            }
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            count++;
+        }
+    }
+    
+    return count;
+}
