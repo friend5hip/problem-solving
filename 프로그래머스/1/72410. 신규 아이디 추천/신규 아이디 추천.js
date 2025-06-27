ad0560909ba332666e2f42c9604bf2cb94ef7951
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-06-27 16:45:32 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,24 +1,13 @@
 function solution(new_id) {
-    var answer = new_id.toLowerCase().
-        replace(/[^a-z0-9._-]/g, '');
-    answer = answer.split("");
-    for (let i = 0; i < answer.length - 1;) {
-        if (answer[i] === "." && answer[i + 1] === ".") {
-            answer.splice(i, 1);
-        } else {
-            i++;
-        }
+    var answer = new_id.toLowerCase()
+        .replace(/[^a-z0-9._-]/g, '')
+        .replace(/\.{2,}/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .replace(/^$/, 'a')
+        .replace(/^(.{15}).*$/, '$1')
+        .replace(/\.$/, '')
+    while (answer.length <= 2) {
+        answer += answer[answer.length - 1];
     }
-    if (answer[0] === ".") answer.shift();
-    if (answer[answer.length - 1] === ".") answer.pop();
-    if (answer.length === 0) answer.push("a");
-    if (answer.length >= 16) {
-        answer = answer.slice(0, 15);
-        if (answer[answer.length - 1] === ".") answer[answer.length - 1] = ""; 
-    }
-    while (answer.length < 3) {
-        answer.push(answer[answer.length - 1]);
-    }
-    answer = answer.join("");
     return answer;
 }
