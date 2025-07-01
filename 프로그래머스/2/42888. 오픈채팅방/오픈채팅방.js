8743e6c1cb0a15833af46425123d4254dd88e1e0
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-01 17:35:47 +0900

    [level 2] Title: 오픈채팅방, Time: 322.76 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,24 +1,17 @@
 function solution(record) {
-    const userList = {};
-    for (const log of record) {
-        const [command, uid, nickname] = log.split(" ");
-        if (command === "Enter" && userList[uid] || command === "Change") {
-            userList[uid] = nickname;
-        } else {
-            if (!userList[uid]) {
-                userList[uid] = nickname;
-            }
-        }
+    const userMap = {};
+    const actions = [];
+
+    for (const entry of record) {
+        const [cmd, uid, nick] = entry.split(" ");
+        if (cmd !== "Leave") userMap[uid] = nick;
     }
-    
-    const logs = [];
-    for (const log of record) {
-        const [command, uid, nickname] = log.split(" ");
-        switch (command) {
-            case "Enter": logs.push(`${userList[uid]}님이 들어왔습니다.`); break;
-            case "Leave": logs.push(`${userList[uid]}님이 나갔습니다.`); break;
-        }
+
+    for (const entry of record) {
+        const [cmd, uid] = entry.split(" ");
+        if (cmd === "Enter") actions.push(`${userMap[uid]}님이 들어왔습니다.`);
+        else if (cmd === "Leave") actions.push(`${userMap[uid]}님이 나갔습니다.`);
     }
-    
-    return logs;
+
+    return actions;
 }
