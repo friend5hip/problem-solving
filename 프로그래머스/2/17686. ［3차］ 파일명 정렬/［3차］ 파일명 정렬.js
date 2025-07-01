9dd47f7e525dcb61ca7318e3d27cbb314752e3ab
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-01 17:07:53 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 26.36 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -3,7 +3,7 @@ function solution(files) {
     // 주어진 조건에 따라 파일명을 정렬
     const tokens = [];
     for (const file of files) {
-        const HEAD = file.split(/\d+/)[0];
+        const HEAD = file.split(/\d/)[0];
         const NUMBER = file.match(/\d+/)[0];
         const TAIL = file.substring((HEAD + NUMBER).length);
         tokens.push([HEAD, NUMBER, TAIL]);
@@ -13,6 +13,6 @@ function solution(files) {
         if (a[0].toLowerCase() !== b[0].toLowerCase()) return a[0].localeCompare(b[0]);
         else if (Number(a[1]) !== Number(b[1])) return Number(a[1]) - Number(b[1]);
     })
-    
+
     return tokens.map(token => token.join(""));
 }
