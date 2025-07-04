929a43ef441f8eb61c20e1b3f12ee97e488b759f
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-05 03:36:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 364 ms, Memory: 43532 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,18 @@
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt", "utf8")
+    .trim()
+    .split("\n");
+
+const n = +input[0];
+const users = input.slice(1).map(line => line.split(" "));
+
+users.sort((a, b) => {
+    if (a[1] === a[2]) {
+        return a[1].localeCompare(b[1]);
+    } else {
+        return a[0] - b[0];
+    }
+})
+
+const answer = users.map(line => line.join(" ")).join("\n");
+console.log(answer);
