08e222f6e0f372289ab89056388a21b662998d98
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-04 14:19:47 +0900

    [Silver III] Title: 피보나치 함수, Time: 92 ms, Memory: 9892 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -7,29 +7,16 @@ const input = require("fs")
 const T = +input[0];
 const n = input.slice(1).map(Number);
 
+const dp = Array(41).fill(0);
+dp[0] = [1, 0];
+dp[1] = [0, 1];
+
+for (let i = 2; i <= 40; i++) {
+    const [a, b] = dp[i - 1];
+    const [c, d] = dp[i - 2];
+    dp[i] = [a + c, b + d];
+}
+
 for (let t = 0; t < T; t++) {
-    const dp = Array(n[t] + 1).fill(0);
-    dp[0] = [0, 1];
-    dp[1] = [1, 0];
-    dp[2] = [1, 1];
-    dp[3] = [1, 2];
-
-    if (n[t] === 0) {
-        console.log(1, 0); 
-        continue;
-    } else if (n[t] === 1) {
-        console.log(0, 1);
-        continue;
-    } else if (n[t] === 3) {
-        console.log(1, 2);
-        continue;
-    }
-
-    for (let i = 4; i <= n[t]; i++) {
-        const [a, b] = dp[i - 1];
-        const [c, d] = dp[i - 2];
-        dp[i] = [a + c, b + d];
-    }
-
     console.log(dp[n[t]][0], dp[n[t]][1]);
 }
