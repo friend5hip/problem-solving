6f0e6c54e5538b8d7d0bb8d4955f0a908b1849cc
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-07 01:31:27 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,5 @@
 function solution(array, commands) {
-    const answer = [];
-    for (const [i, j ,k] of commands) {
-        const temp = [];
-        for (let e = i; e <= j; e++) {
-            temp.push(array[e - 1]);
-        }
-        temp.sort((a, b) => a - b);
-        answer.push(temp[k - 1]);
-    }
-    return answer;
+    return commands.map(([i, j, k]) => 
+        array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
+    );
 }
