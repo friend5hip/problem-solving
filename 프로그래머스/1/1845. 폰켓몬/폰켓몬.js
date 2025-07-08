4ed30061eda14a26d46e61db2317fc6904b38653
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-08 12:30:40 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,4 @@
 function solution(nums) {
-    const map = new Map();
-    for (const num of nums) {
-        map.set(num, (map.get(num) || 0) + 1);
-    }
-    return Math.min(Math.floor(nums.length / 2), map.size);
+    const set = new Set([...nums]);
+    return Math.min(Math.floor(nums.length / 2), set.size);
 }
