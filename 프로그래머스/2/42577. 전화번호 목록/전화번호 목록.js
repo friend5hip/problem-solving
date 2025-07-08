4ea005f317a127cf076ead466a5014768c373cf3
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-08 13:00:43 +0900

    [level 2] Title: 전화번호 목록, Time: 156.80 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,8 @@
 function solution(phone_book) {
-    phone_book = phone_book.sort((a, b) => a.localeCompare(b));
+    phone_book = phone_book.sort();
     for (let i = 1; i < phone_book.length; i++) {
         if (phone_book[i].startsWith(phone_book[i - 1])) {
             return false;
-            break;
         }
     }
     return true;
