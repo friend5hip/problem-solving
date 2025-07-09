799343483e0a401ba79fb2b67a78895f28638483
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-09 13:59:51 +0900

    [level 2] Title: 기능개발, Time: 0.61 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,19 +1,16 @@
 function solution(progresses, speeds) {
-    const progQueue = [...progresses];
-    const speedQueue = [...speeds];
     const answer = [];
 
-    while (progQueue.length > 0) {
+    while (progresses.length > 0) {
         // 개발 진행
-        for (let i = 0; i < progQueue.length; i++) {
-            if (progQueue[i] >= 100) continue;
-            progQueue[i] += speedQueue[i];
+        for (let i in progresses) {
+            if (progresses[i] < 100) progresses[i] += speeds[i];
         }
         // 맨 앞 기능 개발 완료 체크
         let count = 0;
-        while (progQueue[0] >= 100) {
-            progQueue.shift();
-            speedQueue.shift();
+        while (progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
             count++;
         }
         if (count > 0) answer.push(count);
