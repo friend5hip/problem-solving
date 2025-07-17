0a01ca29e49aac53017ce45b79717a8bd8dd3f6c
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-17 14:40:32 +0900

    [level 1] Title: 최소직사각형, Time: 32.11 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,22 +1,12 @@
 function solution(sizes) {
-    let [maxW, maxH] = sizes[0];
-    for (let i = 1; i < sizes.length; i++) {
-        const [w, h] = sizes[i];
-        if ((w < maxW && h < maxH) || (h < maxW && w < maxH)) continue;
-        else {
-            if (Math.max(w, maxW) * Math.max(h, maxH) < Math.max(h, maxW) * Math.max(w, maxH)) {
-                maxW = Math.max(w, maxW);
-                maxH = Math.max(h, maxH);
-            } else {
-                maxW = Math.max(h, maxW);
-                maxH = Math.max(w, maxH);
-            }
-        }
+    let maxW = 0;
+    let maxH = 0;
+    
+    for (const [w, h] of sizes) {
+        const [longer, shorter] = w > h ? [w, h] : [h, w];
+        maxW = Math.max(maxW, longer);
+        maxH = Math.max(maxH, shorter);
     }
     
     return maxW * maxH;
 }
-
-// 제일 큰 가로, 세로로 만들면 모든 명함 수납 가능
-// 하지만 명함을 회전 시켜봤을 때 더 작은 가로, 세로가 가능하다면 그 길이를 선택
-// 하나씩 가능한 길이를 겹쳐보면서 최대 가로, 세로 길이를 갱신
