74fccbb9bce823903f7f1164a43a59fc664a0dbb
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-17 15:17:11 +0900

    [level 1] Title: 모의고사, Time: 3.63 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -2,24 +2,21 @@ function solution(answers) {
     const first = [1, 2, 3, 4, 5];
     const second = [2, 1, 2, 3, 2, 4, 2, 5];
     const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
-    const count = new Map();
+
+    const count = [0, 0, 0];
+
     for (let i = 0; i < answers.length; i++) {
-        const answer = answers[i];
-        if (first[i % 5] === answer) count.set(1, (count.get(1) || 0) + 1);
-        if (second[i % 8] === answer) count.set(2, (count.get(2) || 0) + 1);
-        if (third[i % 10] === answer) count.set(3, (count.get(3) || 0) + 1);
+        if (answers[i] === first[i % first.length]) count[0]++;
+        if (answers[i] === second[i % second.length]) count[1]++;
+        if (answers[i] === third[i % third.length]) count[2]++;
     }
-    
-    const sorted = [...count.entries()].sort((a, b) => {
-        if (a[1] === b[1]) return a[0] - b[0];
-        return b[1] - a[1];
-    });
-    
-    let max = sorted[0][1];
-    const answer = [];
-    for (let [i, c] of sorted) {
-        if (max === c) answer.push(i);
+
+    const max = Math.max(...count);
+    const result = [];
+
+    for (let i = 0; i < 3; i++) {
+        if (count[i] === max) result.push(i + 1);
     }
-    return answer;
+
+    return result;
 }
