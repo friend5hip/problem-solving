c0efbae46c221f659c33ce9ea1d09a47630ae527
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-18 15:04:47 +0900

    [level 2] Title: 소수 찾기, Time: 6.82 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,11 +1,31 @@
 function solution(numbers) {
-    numbers = numbers.split("").map(e => Number(e));
-    const allNumbers = getCombs(numbers);
-    console.log(allNumbers);
+    numbers = numbers.split("").map(Number);
+    const result = new Set();
+    const used = Array(numbers.length).fill(false);
+
+    function dfs(path) {
+        if (path.length > 0) {
+            result.add(Number(path.join("")));
+        }
+
+        for (let i = 0; i < numbers.length; i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            path.push(numbers[i]);
+            dfs(path);
+            path.pop();
+            used[i] = false;
+        }
+    }
+
+    dfs([]);
+
     let count = 0;
-    for (const number of allNumbers) {
+    for (const number of result) {
         if (isPrime(number)) count++;
     }
+
     return count;
 }
 
@@ -15,28 +35,4 @@ function isPrime(number) {
         if (number % i === 0) return false;
     }
     return true;
-}
-
-function getCombs(numbers) {
-    const result = new Set();
-
-    function dfs(path, used) {
-            if (path.length > 0) {
-        result.add(Number(path.join('')));
-        }
-
-        for (let i = 0; i < numbers.length; i++) {
-        if (used[i]) continue;
-
-        used[i] = true;
-        path.push(numbers[i]);
-        dfs(path, used);
-        path.pop();
-        used[i] = false;
-        }
-    }
-
-    dfs([], Array(numbers.length).fill(false));
-
-    return Array.from(result).sort((a, b) => a - b);
 }
