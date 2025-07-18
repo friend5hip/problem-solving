dbcffe23a28f956455de417a412f7e1d7f53aa27
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-18 16:58:47 +0900

    [level 2] Title: 카펫, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,13 +1,12 @@
 function solution(brown, yellow) {
     const total = brown + yellow;
-    for (let b = Math.floor(brown / 2); b >= 1; b--) {
-        for (let y = 1; y <= yellow; y++) {
-            if (b - 2 !== y) continue;
-            const bHeight = (brown - b * 2) / 2 + 2 > 0 ? (brown - b * 2) / 2 + 2 : 1;
-            const yHeight = (yellow - y * 2) / 2 > 0 ? (yellow - y * 2) / 2 + 2 : 1;
-            if ((b * bHeight) === total) {
-                return [b, bHeight];
-            }
+
+    for (let height = 3; height <= total; height++) {
+        if (total % height !== 0) continue;
+        const width = total / height;
+
+        if ((width - 2) * (height - 2) === yellow) {
+            return [width, height];
         }
     }
 }
