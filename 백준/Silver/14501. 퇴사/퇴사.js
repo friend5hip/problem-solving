94e4633000701e1ea7eb0ed48f3e9fd77bf8acd4
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-21 15:40:31 +0900

    [Silver III] Title: 퇴사, Time: 160 ms, Memory: 11188 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -0,0 +1,25 @@
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt", "utf-8")
+    .trim()
+    .split("\n")
+
+const n = +input[0];
+const council = input.slice(1).map(e => e.split(" ").map(Number));
+
+let max = 0;
+
+function dfs(day, profit) {
+    if (day >= n) {
+        max = Math.max(max, profit);
+        return;
+    }
+
+    const [t, p] = council[day];
+    if (day + t <= n) {
+        dfs(day + t, profit + p);
+    }
+    dfs(day + 1, profit);
+}
+
+dfs(0, 0);
+console.log(max);
