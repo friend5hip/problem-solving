b98df9168be2fd2892573f22964e6f809c092acd
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-23 22:38:41 +0900

    [Silver II] Title: N과 M (12), Time: 156 ms, Memory: 11364 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt", "utf8")
+    .trim()
+    .split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number).sort((a, b) => a - b);
+const result = [];
+const set = new Set();
+
+function dfs(current, index) {
+    if (current.length === m) {
+        const comb = current.join(" ");
+        if (!set.has(comb)) {
+            set.add(comb);
+            result.push(comb);
+        }
+        return;
+    }
+
+    for (let i = index; i < n; i++) {
+        current.push(arr[i]);
+        dfs(current, i);
+        current.pop();
+    }
+}
+
+dfs([], 0);
+console.log(result.join("\n"));
