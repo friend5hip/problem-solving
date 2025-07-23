760fc0259e0084dad4a92ab512f4addb1c56e99a
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-23 23:02:52 +0900

    [Gold IV] Title: N-Queen, Time: 3312 ms, Memory: 10652 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.js
+++ 백준/Gold/9663. N－Queen/N－Queen.js
@@ -7,22 +7,22 @@ const n = +input[0];
 const cols = Array(n).fill(false);
 const diag1 = Array(2 * n - 1).fill(false);
 const diag2 = Array(2 * n - 1).fill(false);
+let count = 0;
 
 function backtrack(row) {
     if (row === n) {
-        return 1;
+        count++;
+        return;
     }
 
-    let count = 0;
     for (let col = 0; col < n; col++) {
         if (!cols[col] && !diag1[row + col] && !diag2[row - col + (n - 1)]) {
             cols[col] = diag1[row + col] = diag2[row - col + (n - 1)] = true;
-            count += backtrack(row + 1);
+            backtrack(row + 1);
             cols[col] = diag1[row + col] = diag2[row - col + (n - 1)] = false;
         }
     }
-
-    return count;
 }
 
-console.log(backtrack(0));
+backtrack(0);
+console.log(count);
