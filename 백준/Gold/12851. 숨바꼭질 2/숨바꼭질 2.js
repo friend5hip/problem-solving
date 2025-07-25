5b73891c04a2c058254f4bcecc2ee2df226c36e4
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-26 00:41:14 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 304 ms, Memory: 48280 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.js
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.js
@@ -8,21 +8,17 @@ const Queue = () => {
     let start = 0;
     let end = 0;
 
-    const add = (e) => {
-        queue.push(e);
-        end++;
-    }
-
-    const poll = () => {
-        return queue[start++];
-    }
-
     return {
-            add, 
-            poll, 
-            get length() {
-                return end - start;
-            }
+        add: (e) => {
+            queue.push(e);
+            end++;
+        },
+        poll: () => {
+            return queue[start++];
+        },
+        get length() {
+            return end - start;
+        }
     }
 }
 
@@ -32,34 +28,35 @@ let count = 0;
 let min = -1;
 
 bfs(n);
-console.log(min);
-console.log(count);
+
+console.log(min + "\n" + count);
 
 function bfs(start) {
     const visited = Array(100001).fill(-1);
     const q = Queue();
     q.add([start, 0]);
+    visited[start] = 0;
 
     while (q.length > 0) {
-        const [current, currentTime] = q.poll();
+        const [current, time] = q.poll();
         
         if (current === k) {
             if (min === -1) {
-                min = currentTime;
+                min = time;
                 count = 1;
-            } else if (currentTime === min) {
+            } else if (time === min) {
                 count++;
             }
             continue;
         }
 
-        if (min !== -1 && currentTime > min) break;
+        if (min !== -1 && time > min) break;
 
         for (const next of [current - 1, current + 1, current * 2]) {
             if (next < 0 || next > 100000) continue;
-            if (visited[next] === -1 || visited[next] === currentTime + 1) {
-                q.add([next, currentTime + 1]);
-                visited[next] = currentTime + 1;
+            if (visited[next] === -1 || visited[next] === time + 1) {
+                q.add([next, time + 1]);
+                visited[next] = time + 1;
             }
         }
     }
