856768e439af7074a15493f7008dd464b25d92c1
Jeon Woojeong <107809044+friend5hip@users.noreply.github.com>
2025-07-30 00:17:24 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2192 ms, Memory: 325220 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -3,42 +3,24 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const Queue = () => {
-    const q = [];
-    let head = 0;
-    let tail = 0;
-
-    return {
-        add: (value) => {
-            q.push(value);
-            tail++;
-        },
-        poll: () => {
-            return q[head++];
-        },
-        get length() {
-            return tail - head;
-        }
-    }
-}
-
 const [n, m] = input[0].split(" ");
 const map = input.slice(1).map(e => e.split("").map(Number));
 const visited = Array.from({ length: n }, () => Array.from({ length: m }, () => Array(2).fill(false)));
 const dx = [1, -1, 0, 0];
 const dy = [0, 0, -1, 1];
 
+console.log(bfs());
+
 function bfs() {
-    const q = Queue();
-    q.add({ x: 0, y: 0, wallBroken: 0, dist: 1 });
+    const q = [];
+    q.push([0, 0, 0, 1]);
     visited[0][0][0] = true;
 
-    while (q.length > 0) {
-        const { x, y, wallBroken, dist } = q.poll();
+    let head = 0;
+    while (q.length > head) {
+        const [x, y, wallBroken, dist] = q[head++];
 
-        if (x === n - 1 && y === m - 1) {
-            return dist;
-        }
+        if (x === n - 1 && y === m - 1) return dist;
 
         for (let i = 0; i < 4; i++) {
             const nx = x + dx[i];
@@ -47,15 +29,13 @@ function bfs() {
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
             if (map[nx][ny] === 0 && !visited[nx][ny][wallBroken]) {
                 visited[nx][ny][wallBroken] = true;
-                q.add({ x: nx, y: ny, wallBroken: wallBroken, dist: dist + 1 });
-            } else if (map[nx][ny] === 1 && wallBroken === 0) {
+                q.push([nx, ny, wallBroken, dist + 1]);
+            } else if (map[nx][ny] === 1 && wallBroken === 0 && !visited[nx][ny][1]) {
                 visited[nx][ny][1] = true;
-                q.add({ x: nx, y: ny, wallBroken: 1, dist: dist + 1 });
+                q.push([nx, ny, 1, dist + 1]);
             }
         }
     }
 
     return -1;
 }
-
-console.log(bfs());
